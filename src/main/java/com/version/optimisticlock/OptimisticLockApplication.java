1674d5600be44d983b1d021dfad6e47c0145787b
Dzmitry Kashlach <2155941+dzmitrykashlach@users.noreply.github.com>
2022-09-26 12:42:01 +0300

    ObjectOptimisticLockingFailureException

--- src/main/java/com/version/optimisticlock/OptimisticLockApplication.java
+++ src/main/java/com/version/optimisticlock/OptimisticLockApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
 import java.util.concurrent.Executors;
-
+/*
+https://vladmihalcea.com/optimistic-vs-pessimistic-locking/
+ */
 @SpringBootApplication
 public class OptimisticLockApplication implements CommandLineRunner {
 
@@ -18,17 +21,27 @@ public class OptimisticLockApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args){
-        var executor = Executors.newFixedThreadPool(5);
-        for (int i = 1; i < 10; i++) {
-            executor.execute(() -> {
-                cityRepository.findAll();
-                City city = cityRepository.findAll().iterator().next();
-                city.setCountryId("89");
-                cityRepository.save(city);
-                cityRepository.findAll();
-            });
-        }
+    public void run(String... args) {
+        var executor = Executors.newFixedThreadPool(2);
+        Random random = new Random();
+        executor.execute(() -> {
+            City city = cityRepository.findById(1).orElseThrow();
+            city.setCity(Long.toString(random.nextInt(32000)));
+
+            cityRepository.save(city);
+        });
+        executor.execute(() -> {
+            City city = cityRepository.findById(1).orElseThrow();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            city.setCity(Long.toString(random.nextInt(32000)));
+//            ObjectOptimisticLockingFailureException is thrown
+            cityRepository.save(city);
+        });
     }
+
 
 }
